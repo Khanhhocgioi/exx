8fdcea3f2eaddf344c94c8dc4edd0f318e1a1248
Khanhhocgioi <59682474+Khanhhocgioi@users.noreply.github.com>
2020-03-12 15:23:46 +0700

    ok

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var app=express();
 app.set('view engine','jade');
 var port = process.env.PORT || 3000;
 
-var publicDir = require('path').join(__dirname,'/public');
-app.use(express.static(publicDir));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
