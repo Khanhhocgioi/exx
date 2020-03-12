183839863121f87069dbcca0f7fba08329b1bdbc
Khanhhocgioi <59682474+Khanhhocgioi@users.noreply.github.com>
2020-03-12 15:13:47 +0700

    ok

---
+++ server.js
@@ -0,0 +1,13 @@
+var express=require('express');
+var app=express();
+
+app.set('view engine','jade');
+var port = process.env.PORT || 3000;
+
+var publicDir = require('path').join(__dirname,'/public');
+app.use(express.static(publicDir));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var server=app.listen(port,function() {});
